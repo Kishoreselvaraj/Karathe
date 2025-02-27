a2b09ae3c3521e8775ebf98f7041fbf56ee5360c
Kishore Selvaraj <89259172+Kishoreselvaraj@users.noreply.github.com>
2025-02-27 20:33:22 +0530

    /

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+// app.use(express.json());
+const port = process.env.PORT || 3000;
+
+const adminRoutes = require("./routes/admin");
+const studentRoutes = require("./routes/student");
+
+
+const trimRequestBody = require("./middleware/trimBody");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(trimRequestBody);
+
+app.use("/api/admin", adminRoutes);
+app.use("/api/student", studentRoutes)
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
